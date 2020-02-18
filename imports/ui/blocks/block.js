60c79203309d7f24913db0f995e8bc6929342cf0
teddy meksavanh <teddymeksavanh@yahoo.fr>
2020-02-18 10:35:10 +0100

    Before name

--- imports/ui/blocks/block.js
+++ imports/ui/blocks/block.js
@@ -16,8 +16,8 @@ export default class Block extends Component {
             let moniker = (proposer.description&&proposer.description.moniker)?proposer.description.moniker:proposer.address;
             return <Row className="block-info bg-white my-2 py-3 list-border">
                 <Col xs={4} sm={2} className="text-truncate"><i className="material-icons align-middle light-color">layers</i> { this.props.block.hash}</Col>
-                <Col xs={{size:4, offset:8}} sm={{size:2, offset:0}}><i className="fas fa-database d-sm-none"></i> <Link className="primary-color" to={"/blocks/"+this.props.block.height}>{numbro(this.props.block.height).format('0,0')}</Link></Col>
-                <Col xs={8}sm={3} md={2} lg={3} className="text-truncate"><Link className="primary-color" to={"/validator/"+this.props.block.proposerAddress}><Avatar moniker={moniker} profileUrl={proposer.profile_url} address={this.props.block.proposerAddress} list={true} /> {moniker}</Link></Col>
+                <Col xs={{size:4, offset:8}} sm={{size:2, offset:0}}><i className="fas fa-database d-sm-none"></i> <Link className="primary-color font-800" to={"/blocks/"+this.props.block.height}>{numbro(this.props.block.height).format('0,0')}</Link></Col>
+                <Col xs={8}sm={3} md={2} lg={3} className="text-truncate"><Link className="primary-color font-800" to={"/validator/"+this.props.block.proposerAddress}><Avatar moniker={moniker} profileUrl={proposer.profile_url} address={this.props.block.proposerAddress} list={true} /> {moniker}</Link></Col>
                 <Col xs={4} sm={1} md={2}><i className="fas fa-sync d-sm-none"></i> {numbro(this.props.block.transNum).format('0,0')}</Col>
                 <Col xs={8} sm={4} lg={3}><i className="far fa-clock d-sm-none"></i><TimeStamp time={this.props.block.time}/></Col>
             </Row>
